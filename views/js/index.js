1d9c5bad666c49b3abdefac8fb44894b8e608270
Gábor Szántó <szantog82@gmail.com>
2016-11-09 12:27:56 +0100

    Update index.js

--- views/js/index.js
+++ views/js/index.js
@@ -10,3 +10,22 @@ if (username.length > 1) {
   loggedin = false;
 
 }
+
+var arr = ["<p>User Story: As an authenticated user, I can keep my polls and come back later to access them.<p>User Story: As an authenticated user, I can share my polls with my friends.<p>User Story: As an authenticated user, I can see the aggregate results of my polls.<p>User Story: As an authenticated user, I can delete polls that I decide I don't want anymore.<p>User Story: As an authenticated user, I can create a poll with any number of possible items.<p>User Story: As an unauthenticated or authenticated user, I can see and vote on everyone's polls.<p>User Story: As an unauthenticated or authenticated user, I can see the results of polls in chart form. (This could be implemented using Chart.js or Google Charts.)<p>User Story: As an authenticated user, if I don't like the options on a poll, I can create a new option.",
+"<p>User Story: As an unauthenticated user, I can view all bars in my area.<p>User Story: As an authenticated user, I can add myself to a bar to indicate I am going there tonight.<p>User Story: As an authenticated user, I can remove myself from a bar if I no longer want to go there.<p>User Story: As an unauthenticated user, when I login I should not have to search again.",
+"<p>User Story: I can view a graph displaying the recent trend lines for each added stock.<p>User Story: I can add new stocks by their symbol name.<p>User Story: I can remove stocks.<p>User Story: I can see changes in real-time when any other user adds or removes a stock. For this you will need to use Web Sockets.",
+"User Story: I can view all books posted by every user.<p>User Story: I can add a new book.<p>User Story: I can update my settings to store my full name, city, and state.<p>User Story: I can propose a trade and wait for the other user to accept the trade."
+]
+
+$("h3").mouseover(function(){
+    $("#descr").css("visibility", "visible")
+    if (/Voting/.test($(this).text())) $("#descr").html(arr[0])  
+    else if (/Nightlife/.test($(this).text())) $("#descr").html(arr[1])  
+    else if (/Chart/.test($(this).text())) $("#descr").html(arr[2])  
+    else if (/Book/.test($(this).text())) $("#descr").html(arr[3])
+})
+
+$("h3").mouseleave(function(){
+  $("#descr").css("visibility", "hidden")
+})
+
